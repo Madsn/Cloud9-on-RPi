a7f7850b7fd4c25f04eadfba656e9d1b95052a7a
Mikkel Madsen <m@madsn.net>
2016-06-09 23:20:21 +0200

    Upgrade to latest node version
    And run c9 on startup

--- c9_bash_RPi.sh
+++ c9_bash_RPi.sh
@@ -3,7 +3,7 @@
 clear
 
 echo "The script to install Cloud9 on this system starts now"
-echo "This will install node v0.10 and update and upgrade apt-get"
+echo "This will install node and update and upgrade apt-get"
 echo "This may break a few things"
 echo "Here it goes."
 
@@ -11,12 +11,9 @@ cd ~
 
 sudo apt-get update -y && sudo apt-get upgrade -y
 
-echo "We will now get v0.10 of NodeJS"
-wget http://nodejs.org/dist/v0.10.28/node-v0.10.28-linux-arm-pi.tar.gz
-
-cd /usr/local
-
-tar -xzf ~/node-v0.10.28-linux-arm-pi.tar.gz --strip=1
+echo "Install latest NodeJS"
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb 
+sudo dpkg -i node_latest_armhf.deb
 
 export NODE_PATH=”/usr/local/lib/node_modules”
 
@@ -30,6 +27,7 @@ scripts/install-sdk.sh
 
 echo "And now the magic begins"
 echo "------------------------"
+su pi -c 'node /home/pi/c9sdk/server.js -l 0.0.0.0 -a : -w ~ < /dev/null &'
 
 ./server.js -l 0.0.0.0 -a : -w ~
-    
+
